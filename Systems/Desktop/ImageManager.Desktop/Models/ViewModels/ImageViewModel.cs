48ffb6bb50756e9a872a4d6491e53571d19dd6ec
Mr-Keltt <dmitriydavidenkokeltt@gmail.com>
2025-03-25 02:08:34 +0300

    Fixed a bug in rotating tall images.

--- Systems/Desktop/ImageManager.Desktop/Models/ViewModels/ImageViewModel.cs
+++ Systems/Desktop/ImageManager.Desktop/Models/ViewModels/ImageViewModel.cs
@@ -1,6 +1,7 @@
 ﻿// ImageViewModel.cs
 using System.ComponentModel;
 using System.IO;
+using System.Windows.Media;
 using System.Windows.Media.Imaging;
 
 namespace ImageManager.Desktop.Models;
@@ -20,8 +21,8 @@ public class ImageViewModel : INotifyPropertyChanged
         }
     }
 
-    private BitmapImage _imageSource;
-    public BitmapImage ImageSource
+    private ImageSource _imageSource;
+    public ImageSource ImageSource
     {
         get => _imageSource;
         private set
@@ -60,12 +61,53 @@ public class ImageViewModel : INotifyPropertyChanged
         image.CacheOption = BitmapCacheOption.OnLoad;
         image.StreamSource = stream;
         image.EndInit();
-        ImageSource = image;
+
+        var exifOrientation = GetExifOrientation(imageData);
+        if (exifOrientation != 0)
+        {
+            int angle = exifOrientation switch
+            {
+                3 => 180,
+                6 => 270,
+                8 => 90,
+                _ => 0
+            };
+
+            var transform = new RotateTransform(angle);
+            var transformedBitmap = new TransformedBitmap(image, transform);
+            ImageSource = transformedBitmap;
+        }
+        else
+        {
+            ImageSource = image;
+        }
     }
 
     public event PropertyChangedEventHandler? PropertyChanged;
     protected virtual void OnPropertyChanged(string? propertyName = null)
     {
         PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    private static ushort GetExifOrientation(byte[] imageData)
+    {
+        try
+        {
+            using (var ms = new MemoryStream(imageData))
+            {
+                var decoder = BitmapDecoder.Create(ms, BitmapCreateOptions.PreservePixelFormat, BitmapCacheOption.OnLoad);
+                var frame = decoder.Frames[0];
+                var metadata = frame.Metadata as BitmapMetadata;
+                if (metadata != null && metadata.ContainsQuery("/app1/ifd/{ushort=274}"))
+                {
+                    return (ushort)metadata.GetQuery("/app1/ifd/{ushort=274}");
+                }
+            }
+        }
+        catch
+        {
+
+        }
+        return 0;
     }
 }
