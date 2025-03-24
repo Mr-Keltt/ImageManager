fe9d8ce50a8e80a7df91dd1a84442966829a5af3
Mr-Keltt <dmitriydavidenkokeltt@gmail.com>
2025-03-24 16:53:03 +0300

    Added the basic functions and configurations of the project. Dobalen logel, healthchecker, swagger, and so on.

---
+++ Application/Models/ImageManager.Models/Bootstrapper.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace ImageManager.Application.Models;
+
+/// <summary>
+/// Provides a bootstrap mechanism to configure application model mappings using AutoMapper.
+/// </summary>
+public static class Bootstrapper
+{
+    /// <summary>
+    /// Extends the IServiceCollection to register AutoMapper with the application's model mapping profiles.
+    /// </summary>
+    /// <param name="services">The IServiceCollection instance to be configured.</param>
+    /// <returns>The modified IServiceCollection with AutoMapper registration.</returns>
+    public static IServiceCollection AddApplicationModels(this IServiceCollection services)
+    {
+        // Return the updated service collection to allow for method chaining.
+        return services;
+    }
+}
--- Systems/API/ImageManager.API/Program.cs
+++ Systems/API/ImageManager.API/Program.cs
@@ -1,17 +1,47 @@
+using ImageManager.API.Configuration;
+using ImageManager.API;
+using ImageManager.Services.Settings;
+
+var mainSettings = ImageManager.Common.Settings.Settings.Load<MainSettings>("Main");
+var logSettings = ImageManager.Common.Settings.Settings.Load<LogSettings>("Log");
+var swaggerSettings = ImageManager.Common.Settings.Settings.Load<SwaggerSettings>("Swagger");
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.AddAppLogger(mainSettings, logSettings);
 
-builder.Services.AddControllers();
+var services = builder.Services;
+
+services.AddAppController();
+
+services.AddHttpContextAccessor();
+
+//services.AddAppDbContext();
+
+services.AddAppHealthChecks();
+
+services.AddAppSwagger(swaggerSettings);
+
+services.RegisterServicesAndModels();
+
+services.AddAppCors();
+
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+app.UseAppCors();
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
-
+app.UseAppController();
 app.MapControllers();
+
+app.UseAppHealthChecks();
+
+app.UseAppSwagger();
+
+//DbInitializer.Execute(app.Services);
+
+//DbSeeder.Execute(app.Services);
 
 app.Run();
