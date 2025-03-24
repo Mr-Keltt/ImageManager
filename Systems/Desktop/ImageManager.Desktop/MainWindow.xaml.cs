f1693b99d92b6f6c8dd127fc4ea77ec46e7039eb
Mr-Keltt <dmitriydavidenkokeltt@gmail.com>
2025-03-25 00:00:20 +0300

    A project for the desktop part of the application has been created. Completely implemented the appearance of the interface.

---
+++ Systems/Desktop/ImageManager.Desktop/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using ImageManager.Desktop.Models;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ImageManager.Desktop;
+
+public partial class MainWindow : Window
+{
+    public ObservableCollection<ImageItem> Images { get; set; } = new ObservableCollection<ImageItem>();
+
+    public MainWindow()
+    {
+        InitializeComponent();
+        
+        // Пример с абсолютными путями для теста
+        Images.Add(new ImageItem 
+        { 
+            Name = "Привет.png", 
+            ImagePath = @"C:\Users\dmitr\Desktop\Programming\ImageManager\Systems\Desktop\ImageManager.Desktop\bin\Debug\net8.0-windows\placeholder.png"
+        });
+        Images.Add(new ImageItem
+        {
+            Name = "Привет.png",
+            ImagePath = @"C:\Users\dmitr\Desktop\Programming\ImageManager\Systems\Desktop\ImageManager.Desktop\bin\Debug\net8.0-windows\placeholder.png"
+        });
+        
+
+        ImagesGrid.ItemsSource = Images;
+    }
+
+    private void AddButton_Click(object sender, RoutedEventArgs e)
+    {
+        // Логика добавления
+    }
+
+    private void EditButton_Click(object sender, RoutedEventArgs e)
+    {
+        // Логика изменения
+    }
+
+    private void DeleteButton_Click(object sender, RoutedEventArgs e)
+    {
+        // Логика удаления
+    }
+
+    private void ImagesGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        // Обработка выделения строки
+        var selectedItem = ImagesGrid.SelectedItem as ImageItem;
+        // Ваша логика...
+    }
+}
---
+++ Systems/Desktop/ImageManager.Desktop/Models/ImageItem.cs
@@ -0,0 +1,8 @@
+﻿namespace ImageManager.Desktop.Models;
+
+public class ImageItem
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string ImagePath { get; set; }
+}
